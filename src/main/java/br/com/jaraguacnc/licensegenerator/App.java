d785d6ff8f76aeec6e75635f7ca8b3d631b15d2e
gjasper <gustavohj6@gmail.com>
2017-07-04 23:08:37 -0300

    Added encryption tests

--- src/main/java/br/com/jaraguacnc/licensegenerator/App.java
+++ src/main/java/br/com/jaraguacnc/licensegenerator/App.java
@@ -1,13 +1,39 @@
 package br.com.jaraguacnc.licensegenerator;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+    	
+    	String data = "~~~~~~~~";
+    	byte[] cdata = data.getBytes();
+    	String key = "abcdefghijklmnopqrstuvwxyz";
+    	byte[] ckey = key.getBytes();
+        System.out.println( encode(cdata, ckey).toString() );
+        System.out.println( encode(encode(cdata, ckey),ckey).toString() );
+    }
+    
+    public static byte[] rawByteToPrintableAscii(byte[] data){
+    	byte[] outputData = new byte[data.length];
+    	for (int i = 0; i < data.length; i++) {
+    		outputData[i] = (byte) (data[i]+33); 
+		}
+    	return outputData;
+    }
+    
+    public static byte[] printableAsciiToRawByte(byte[] data){
+    	byte[] outputData = new byte[data.length];
+    	for (int i = 0; i < data.length; i++) {
+    		outputData[i] = (byte) (data[i]-33); 
+		}
+    	return outputData;
+    }
+    
+    public static byte[] encode(byte[] data, byte[] key) throws Exception{
+    	byte[] ciphedData = new byte[data.length];
+    	for (int i = 0; i < data.length; i++) {
+    		ciphedData[i] = (byte) (data[i]^key[i]); 
+		}
+    	return ciphedData;
     }
 }
